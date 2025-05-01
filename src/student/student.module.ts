38313ad2309566911c5a64e2f08bd4fd643eb69b
peterdinis <petter1@pobox.sk>
2025-05-01 16:10:17 +0200

    feat: Updating student folder

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "src/prisma/prisma.module";
+import { StudentResolver } from "./student.resolver";
+import { StudentService } from "./student.service";
+import { MailtrapModule } from "src/mailtrap/mailtrap.module";
 
 @Module({
-    imports: [PrismaModule],
-    providers: []
+    imports: [PrismaModule, MailtrapModule],
+    providers: [StudentResolver, StudentService]
 })
 
 export class StudentModule {}
