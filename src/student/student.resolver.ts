6d3658968b29d0605eccc5159723c6aad367af37
peterdinis <petter1@pobox.sk>
2025-05-01 16:17:47 +0200

    feat: Updating student module

--- src/student/student.resolver.ts
+++ src/student/student.resolver.ts
@@ -1,4 +1,32 @@
-import { Resolver } from "@nestjs/graphql";
+import { Resolver, Mutation, Args, Query } from "@nestjs/graphql";
+import { StudentService } from "./student.service";
+import { LoginInput } from "./dto/login-student-input";
+import { RegisterStudentInput } from "./dto/register-student-input";
+import { Student } from "./student.entity";
 
-@Resolver()
-export class StudentResolver {}
+
+@Resolver(() => Student)
+export class StudentResolver {
+  constructor(private readonly studentService: StudentService) {}
+
+  @Mutation(() => Student)
+  async registerStudent(
+    @Args("data") data: RegisterStudentInput
+  ): Promise<Student> {
+    return this.studentService.register(data);
+  }
+
+  @Mutation(() => Student)
+  async loginStudent(
+    @Args("data") data: LoginInput
+  ): Promise<Student> {
+    return this.studentService.login(data);
+  }
+
+  @Query(() => Student)
+  async studentProfile(
+    @Args("id") id: string
+  ): Promise<Student> {
+    return this.studentService.profile(id);
+  }
+}
