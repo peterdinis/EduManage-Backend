06e8a05cb9bebb12c80badcb058fcf547f559943
peterdinis <petter1@pobox.sk>
2025-05-01 16:16:13 +0200

    feat: Working on student module WIP

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
+import { RegisterStudentInput } from "./dto/register-student-input";
+import { LoginInput } from "./dto/login-student-input";
+import * as bcrypt from "bcrypt"
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async register(data: RegisterStudentInput) {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    return this.prisma.student.create({
+      data: {
+        ...data,
+        password: hashedPassword,
+      },
+    });
+  }
+
+  async login(data: LoginInput) {
+    const student = await this.prisma.student.findUnique({
+      where: { email: data.email },
+    });
+
+    if (!student || !(await bcrypt.compare(data.password, student.password))) {
+      throw new UnauthorizedException("Invalid credentials");
+    }
+
+    return student;
+  }
+
+  async profile(studentId: string) {
+    return this.prisma.student.findUnique({ where: { id: studentId } });
+  }
+}
