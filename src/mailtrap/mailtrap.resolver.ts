76366c628d2dbf2038f76f8c8db873c60135a0b6
peterdinis <petter1@pobox.sk>
2025-05-01 14:57:05 +0200

    feat: Building basic mailtrap graphql module

--- src/mailtrap/mailtrap.resolver.ts
+++ src/mailtrap/mailtrap.resolver.ts
@@ -1,4 +1,26 @@
-import { Resolver } from "@nestjs/graphql";
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
+import { MailtrapService } from './mailtrap.service';
+import { BadRequestException } from '@nestjs/common';
+import { SendEmailInput } from './dto/send-email-input.dto';
 
 @Resolver()
-export class MailtrapResolver {}
+export class MailtrapResolver {
+  constructor(private readonly mailtrapService: MailtrapService) {}
+
+  @Mutation(() => Boolean)
+  async sendEmail(
+    @Args('input') input: SendEmailInput
+  ): Promise<boolean> {
+    const success = await this.mailtrapService.sendMail(
+      input.to,
+      input.subject,
+      input.message
+    );
+
+    if (!success) {
+      throw new BadRequestException('Failed to send email.');
+    }
+
+    return true;
+  }
+}
