6603fb09d313ba6ed0710df4d6c615633c7acf77
peterdinis <petter1@pobox.sk>
2025-05-02 19:48:12 +0200

    feat: Updating student.module

---
+++ src/student/entities/class.entity.ts
@@ -0,0 +1,13 @@
+import { Field, Int } from "@nestjs/graphql";
+import { Subject } from "./subject.entity";
+
+export class Class {
+    @Field(() => Int)
+    id: number;
+  
+    @Field()
+    name: string;
+  
+    @Field(() => Subject)
+    subject: Subject;
+  }
