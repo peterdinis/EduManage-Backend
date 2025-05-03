0db11a8fb31416b88c248d2864726ea41bce5399
peterdinis <petter1@pobox.sk>
2025-05-03 19:08:50 +0200

    infra: Enable graphql subscriptions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,9 @@ import { TeacherModule } from 'src/teacher/teacher.module';
       driver: ApolloDriver,
       debug: true,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      subscriptions: {
+        "graphql-ws": true
+      }
     }),
     PrismaModule,
     MailtrapModule,
