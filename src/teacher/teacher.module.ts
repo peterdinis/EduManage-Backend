34c6622de97dcf79eda9ff84071de42d4cb09e4e
peterdinis <petter1@pobox.sk>
2025-05-03 11:32:11 +0200

    FOO

--- src/teacher/teacher.module.ts
+++ src/teacher/teacher.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "src/prisma/prisma.module";
+import { TeacherResolver } from "./teacher.resolver";
+import { TeacherService } from "./teacher.service";
 
 @Module({
-    imports: [PrismaModule]
+    imports: [PrismaModule],
+    providers: [TeacherResolver, TeacherService]
 })
 
 export class TeacherModule {}
