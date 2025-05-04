12e4ac8ba7c760a25c60822f1d0572ff124d1da5
peterdinis <petter1@pobox.sk>
2025-05-04 10:24:52 +0200

    feat: Updating teacher module

--- src/teacher/teacher.module.ts
+++ src/teacher/teacher.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { TeacherResolver } from './teacher.resolver';
 import { TeacherService } from './teacher.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, JwtModule.register({
+    secret: process.env.JWT_SECRET || 'supersecret',
+    signOptions: { expiresIn: '1d' },
+  }),],
   providers: [TeacherResolver, TeacherService],
 })
 export class TeacherModule {}
