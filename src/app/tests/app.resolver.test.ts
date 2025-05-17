a0ecc22a3d5a0b75e884acfd938f036ce9cc0913
peterdinis <petter1@pobox.sk>
2025-05-17 19:58:58 +0200

    test: New unit test for app.resolver

---
+++ src/app/tests/app.resolver.test.ts
@@ -0,0 +1,13 @@
+import { AppResolver } from "../app.resolver";
+
+describe('AppResolver', () => {
+  let resolver: AppResolver;
+
+  beforeEach(() => {
+    resolver = new AppResolver();
+  });
+
+  it('should return "Hello Edu Manage!"', () => {
+    expect(resolver.hello()).toBe('Hello Edu Manage!');
+  });
+});
